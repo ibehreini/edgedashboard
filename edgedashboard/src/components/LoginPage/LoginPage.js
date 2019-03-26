d2456fcfbeb5142e514acec3580b19dc3b18204f
ibehreini <behreini.ida@gmail.com>
2019-03-26 19:12:42 -0400

    -Created Staff, Mentor, and Student Pages -Routed pages so that they can be accessed with the URL -Created Login Page -Re-arranged file structure according to react best practes

---
+++ edgedashboard/src/components/LoginPage/LoginPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class LoginPage extends Component {
+    render() {
+      return (
+        <div className="HomePage">
+          <h2>Everyone will login here!</h2>
+        </div>
+      );
+    }
+  }
+  
+export default LoginPage;
---
+++ edgedashboard/src/components/StaffPage/StaffPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class StaffPage extends Component {
+  render() {
+    return (
+      <div className="StaffPage">
+        <h2>Staff stuff will be here!</h2>
+      </div>
+    );
+  }
+}
+
+export default StaffPage;
