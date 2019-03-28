74bbc36c5e8a2e13a8c356555a3954558699865d
ibehreini <behreini.ida@gmail.com>
2019-03-28 15:53:51 -0400

    Added simple tole naviation menu for staff pae

--- edgedashboard/src/components/StaffPage/StaffPage.js
+++ edgedashboard/src/components/StaffPage/StaffPage.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import NavBar from '../NavBar/NavBar';
 
 class StaffPage extends Component {
   render() {
     return (
       <div className="StaffPage">
+        <NavBar />
         <h2>Staff stuff will be here!</h2>
       </div>
     );
