4ba26fcf6eaa5e8f10a596bb5cfedf7fca5794ae
Henry Gidings <henrygiddings7@gmail.com>
2019-04-14 12:19:29 -0400

    Decreased indentation for readability

--- database-scripts/makedb.sh
+++ database-scripts/makedb.sh
@@ -6,9 +6,9 @@
 #Find out where psql (likely) is
 unameOut="$(uname -s)"
 case "${unameOut}" in
-    Darwin*)    psql="/Applications/Postgres.app/Contents/Versions/11/bin/psql";;
-    CYGWIN*)    psql="src/bin/psql";;
-    *)          psql="psql";; #guess that this is a valid command
+    Darwin*)  psql="/Applications/Postgres.app/Contents/Versions/11/bin/psql";;
+    CYGWIN*)  psql="src/bin/psql";;
+    *)        psql="psql";; #guess that this is a valid command
 esac
 
 
