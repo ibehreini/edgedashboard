b782ab6da9e520625ddccce1fabf9676fd8c898a
Henry Gidings <henrygiddings7@gmail.com>
2019-04-15 14:55:23 -0400

    Fixed models/API broken by mismatch with schema.

--- server/models/events.js
+++ server/models/events.js
@@ -2,7 +2,7 @@ const db = require('../database');
 
 class events {
   static retrieveAll (callback) {
-    db.query('SELECT title, eventdate from events', (err, res) => {
+    db.query('SELECT title, eventTime from events', (err, res) => {
       if (err.error)
         return callback(err);
       callback(res);
@@ -10,7 +10,7 @@ class events {
   }
 
   static retrieveAllWorkshops (wStr, callback) {
-    db.query('SELECT title, eventdate from events where eventtype = $1 ', [wStr], (err, res) => {
+    db.query('SELECT title, eventTime from events where eventtype = $1 ', [wStr], (err, res) => {
       if (err.error)
         return callback(err);
       callback(res);
@@ -18,7 +18,7 @@ class events {
   }
 
   static insert (title, eventtime, callback) {
-    db.query('INSERT INTO events (title, eventdate) VALUES ($1,$2)', [title], [eventdate], (err, res) => {
+    db.query('INSERT INTO events (title, eventTime) VALUES ($1,$2)', [title], [eventTime], (err, res) => {
       if (err.error)
         return callback(err);
       callback(res);
