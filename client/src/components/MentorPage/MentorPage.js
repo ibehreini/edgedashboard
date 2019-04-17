50048d88d8d485aa1d81d86fab8c9c9cd569153d
pasibe1 <pasibe1628634@gmail.com>
2019-04-16 21:16:50 -0400

    First draft changes. All input fields are wrapped in a form element. All elements should be responsive when the page is resized on desktop. Pay no attention to the colors of divs and other elements. They're just there to aid development.

--- client/src/components/MentorPage/MentorPage.js
+++ client/src/components/MentorPage/MentorPage.js
@@ -1,13 +1,5 @@
-import React, { Component } from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+import MAIN from "./app.jsx";
 
-class MentorPage extends Component {
-  render() {
-    return (
-      <div className="MentorPage">
-        <h2>Mentor stuff will be here! 3 text boxes + month picker</h2>
-      </div>
-    );
-  }
-}
-
-export default MentorPage;
+ReactDOM.render(<MAIN />, document.getElementById("root"));
---
+++ client/src/components/MentorPage/app.jsx
@@ -0,0 +1,121 @@
+import React from "react";
+
+class LEFT_SPAN extends React.Component {
+  render() {
+    var divStyle = {
+      background: "blue",
+      height: "100%",
+      width: "50%",
+      display: "inline-flex"
+    };
+    return <span style={divStyle} />;
+  }
+}
+
+class RIGHT_SPAN extends React.Component {
+  render() {
+    var divStyle = {
+      background: "red",
+      height: "100%",
+      width: "50%",
+      display: "inline-flex"
+    };
+    var wrapDivStyle = {
+      textAlign: "center",
+      height: "100%",
+      width: "100%",
+      background: "yellow"
+    };
+    var formStyle = {
+      position: "absolute",
+      background: "green",
+      height: "84vh",
+      width: "50vw"
+    };
+    var hoursLabelStyle = {
+      display: "inline"
+    };
+    var hoursInputStyle = {
+      marginTop: "13%",
+      marginBottom: "15%"
+    };
+    var justifyLabelStyle = {
+      display: "inline"
+    };
+    var justifyInputStyle = {
+      marginBottom: "15%"
+    };
+    var notesLabelStyle = {
+      display: "block"
+    };
+    var hours = "Hours: ";
+    var justify = "Justify: ";
+    var notes = "Notes on Student: ";
+    return (
+      <span style={divStyle}>
+        <div style={wrapDivStyle}>
+          <form style={formStyle}>
+            <label for="hours" style={hoursLabelStyle}>
+              {hours}
+            </label>
+            <input
+              style={hoursInputStyle}
+              type="text"
+              name="hours"
+              id="hours"
+              placeholder="Hours Worked"
+            />
+
+            <br />
+
+            <label for="justify" style={justifyLabelStyle}>
+              {justify}
+            </label>
+            <input
+              style={justifyInputStyle}
+              type="text"
+              name="justify"
+              id="justify"
+              placeholder="Justify Hours"
+            />
+
+            <label for="justify" style={notesLabelStyle}>
+              {notes}
+            </label>
+            <textarea
+              name="notes"
+              id="notes"
+              placeholder="Enter notes here"
+              rows="5"
+              cols="40"
+            />
+          </form>
+        </div>
+      </span>
+    );
+  }
+}
+
+class MAIN extends React.Component {
+  render() {
+    var mainStyle = {
+      background: "lightblue",
+      height: "84vh",
+      width: "100vw"
+    };
+
+    var titleStyle = {
+      background: "green",
+      height: "7%"
+    };
+    return (
+      <div style={mainStyle}>
+        <h1 style={titleStyle}>Mentors</h1>
+        <LEFT_SPAN />
+        <RIGHT_SPAN />
+      </div>
+    );
+  }
+}
+
+export default MAIN;
