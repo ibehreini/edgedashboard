e4a8d3b076026d4e06696aeef0c222f94fd17055
Henry Gidings <henrygiddings7@gmail.com>
2019-05-02 12:07:48 -0400

    Added some needed methods

--- server/models/role.js
+++ server/models/role.js
@@ -1,20 +1,29 @@
 const db = require('../database');
 
 const querycallback = (err, res) =>
-                          err.error ? callback( err ) : callback( res);
+                          err.error ? callback( err ) : callback( res),
+      table = db.queryMaker( 'Role' );
 
 class Role
 {
+  static usersByRole( role, callback )
+  {
+    const qstring = table.select().where( 'role', role ).toString();
+    db.query( qstring, querycallback );
+  }
+  static allUsers( callback )
+  {
+    const qstring = table.select().toString();
+    db.query( qstring, querycallback );
+  }
   static role( userKey, callback )
   {
-    const qstring = db.queryMaker().select().from( 'Role' ).where(
-                      'email', userKey ).toString();
+    const qstring = table.select().where( 'email', userKey ).toString();
     db.query( qstring, querycallback );
   }
   static addRole( userKey, role, callback )
   {
-    const qstring = db.queryMaker().insert( {userKey, role} ).into( 'Role'
-                      ).toString();
+    const qstring = table.insert( {userKey, role} ).into( 'Role' ).toString();
     db.query( qstring, querycallback );
   }
 }
