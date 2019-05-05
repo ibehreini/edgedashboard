b563f78a626fbdd4f5bdb3ac0f2b790073d11b9c
Henry Gidings <henrygiddings7@gmail.com>
2019-05-05 13:23:07 -0400

    Added Logout button

--- client/src/components/MentorPage/MentorPage.js
+++ client/src/components/MentorPage/MentorPage.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import ControlledExpansionPanels from '../StudentPage/ControlledExpansionPanels';
+import LoginPanel from '../StudentPage/LoginPanel';
 
 class MentorPage extends Component {
   render() {
     return (
       <div className="MentorPage">
         <h2>Mentor stuff will be here! 3 text boxes + month picker</h2>
-        // {this.props.email}
+        <LoginPanel />
+         {localStorage.getItem('isLoggedIn')}
+         <ControlledExpansionPanels email={this.props.email} />
       </div>
     );
   }
