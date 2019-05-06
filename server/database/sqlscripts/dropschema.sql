a740a5131cee6fccd556db91b09e087ad987e1b4
Henry Gidings <henrygiddings7@gmail.com>
2019-05-06 17:01:10 -0400

    Moved things around for client branch

--- server/database/sqlscripts/dropschema.sql
+++
@@ -1,6 +0,0 @@
-/*BEWARE: This will drop the ENTIRETY of the database's current schema INCLUDING
- * all declared types AND any existing data!  This should ONLY be used to test
- * the schema creation script, NOT to delete test data!  Use cleartables for
- * that.*/
-DROP SCHEMA public CASCADE;
-CREATE SCHEMA public;
